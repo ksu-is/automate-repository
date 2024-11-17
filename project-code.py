22b49b4c9064d12c7462189135577d662b6088b2
MichaelCato28 <mcato10@students.kennesaw.edu>
2024-11-17 17:51:48 -0500

    Added "Commit" comments
    Updated code to show commits split into sections.

--- project-code.py
+++ project-code.py
@@ -1,6 +1,7 @@
 # This file will store and track changes to the code for AutoMate
 
 #AutoMate, a vehicle booking system developed by Michael Cato. Inspiration drawn from cloned code repository. 
+#Commit 1 below
 print("Welcome to AutoMate! Your car's best booking friend!")
 def appointment_booking(appointments):
     print("Booking a new appointment?")
@@ -8,11 +9,13 @@ def appointment_booking(appointments):
     vehicle_make = input("Enter vehicle make: ")
     vehicle_model = input("Enter vehicle model: ")
     service = []
+#Commit 2 below
 while True:
         service_items = input("Please enter requested service, or enter 'done' to complete: ")
         if service_items.lower() == 'done':
             break
         service.append(service_items)
+#Commit 3 below
 appointment = {
         'year': vehicle_year,
         'make': vehicle_make,
@@ -21,6 +24,7 @@ appointment = {
     }
     appointments.append(appointment)
     print(f"Great! New appointment booked for {vehicle_year} {vehicle_make} {vehicle_model} ! The services included are: {', '.join(service_items)}")
+#Commit 4 below
 def add_new(appointments):
     vehicle_year = input("Enter the car's year: ")
     vehicle_make = input("Enter the car's make: ")
