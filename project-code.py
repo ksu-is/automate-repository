c486e1a3f0c3083499956f3dc48a5ed45a6263b2
MichaelCato28 <mcato10@students.kennesaw.edu>
2024-11-17 17:41:08 -0500

    Added appointment booking function
    Added appointment booking function, this allows for the input of vehicle details and service items. It also allows for printing of prompts.

--- project-code.py
+++ project-code.py
@@ -1,1 +1,10 @@
 # This file will store and track changes to the code for AutoMate
+
+#AutoMate, a vehicle booking system developed by Michael Cato. Inspiration drawn from cloned code repository. 
+print("Welcome to AutoMate! Your car's best booking friend!")
+def appointment_booking(appointments):
+    print("Booking a new appointment?")
+    vehicle_year = input("Enter vehicle year: ")
+    vehicle_make = input("Enter vehicle make: ")
+    vehicle_model = input("Enter vehicle model: ")
+    service = []
